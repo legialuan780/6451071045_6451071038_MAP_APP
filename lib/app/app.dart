102e80cafe6d2d46a8b868d01c2a1d4e3c03ff36
legialuan780 <legialuan780@gmail.com>
2026-05-05 16:14:33 +0700

    fix bug app

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import '../routes/app_routes.dart';
 
 class MyApp extends StatelessWidget {
